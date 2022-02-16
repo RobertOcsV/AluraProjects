67e5742572733dacb48c2bd9d23bafa3b46527f7
rob <robertoliveiracor@hotmail.com>
2022-02-16 01:45:23 -0300

    att

--- projetodomjs/listaDeTarefas.js
+++ projetodomjs/listaDeTarefas.js
@@ -1,13 +1,13 @@
-const cfiarTarefa
+const criarTarefa = (evento) => {
+
+    evento.preventDefault()
+
+    const input = document.querySelector('[data-form-input]');
+    const valor = input.value
+    console.log (valor)
+    input.value= " "
+}
 
 const novaTarefa =  document.querySelector('[data-form-button]');
 
-novaTarefa.addEventListener('click', (evento)=> {
-
-    evento.preventDefault()
-    const input = document.querySelector('[data-form-input]');
-    const valor = input.value
-
-    console.log (valor)
-    input.value= " "
-});
+novaTarefa.addEventListener('click', criarTarefa )
