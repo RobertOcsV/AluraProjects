3cc08fddee249d2b4e4e9d47925590bdd5994a12
Robert <61297366+RobertOcsV@users.noreply.github.com>
2022-04-08 12:46:35 -0300

    adicionando novo componente ao módulo do angular.

--- bytebank/src/app/app.module.ts
+++ bytebank/src/app/app.module.ts
@@ -1,3 +1,4 @@
+import { NovaTransferenciaComponents } from './nova-tranferencia/nova-transferencia.component';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
@@ -5,7 +6,8 @@ import { AppComponent } from './app.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NovaTransferenciaComponents
   ],
   imports: [
     BrowserModule
