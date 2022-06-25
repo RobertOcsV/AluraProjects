b1e2e67fe24267533b179281ee6d1061d2acc8d5
Robert <61297366+RobertOcsV@users.noreply.github.com>
2022-06-25 15:38:09 -0300

    inicio do projeto alura curso bootstrap

--- bytebank/src/app/app.component.ts
+++ bytebank/src/app/app.component.ts
@@ -8,7 +8,11 @@ import { Component } from '@angular/core';
 export class AppComponent {
   title = 'bytebank';
 
+    transferencia: any = {};
+
     transferir($event){
       console.log($event);
+
+      this.transferencia = $event;
     }
 }
