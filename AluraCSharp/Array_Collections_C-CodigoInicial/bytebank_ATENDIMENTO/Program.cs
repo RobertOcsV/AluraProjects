a52b1f181b290a73eed16b0f2078ec60ea94a873
Robert <61297366+RobertOcsV@users.noreply.github.com>
2022-09-22 13:09:49 -0300

    arraysCurso

--- AluraCSharp/Array_Collections_C-CodigoInicial/bytebank_ATENDIMENTO/Program.cs
+++ AluraCSharp/Array_Collections_C-CodigoInicial/bytebank_ATENDIMENTO/Program.cs
@@ -26,32 +26,3 @@
 
 
 
-void TestaBuscarPalavras() { 
-    string[] arrayDePalavras = new string[5];
-
-    for (int i = 0; i < arrayDePalavras.Length; i++)
-    {
-        Console.WriteLine($"Digite {i+1} Palavra: ");
-        arrayDePalavras[i] = Console.ReadLine();
-
-    }
-
-    Console.WriteLine("Digite palavra a ser encontrada: ");
-    var busca = Console.ReadLine();
-
-    foreach (string palavra in arrayDePalavras)
-    {
-        if (palavra.Equals(busca))
-        {
-            Console.WriteLine($"Palavra encontrada = {busca}");
-            break;
-        }
-        else 
-        {
-            Console.WriteLine($"Palavra não encontrada");
-        }
-
-
-    }
-
-}
---
+++ AluraCSharp/CSharpArray/ConsoleApp1/Array3/Program.cs
@@ -0,0 +1,31 @@
+﻿
+
+
+Array amostra = Array.CreateInstance(typeof(double), 5); //outra forma de criar essa array é: new double[5]
+amostra.SetValue(5.9,0);
+amostra.SetValue(1.4,1);
+amostra.SetValue(5.1,2);
+amostra.SetValue(2,3);
+amostra.SetValue(7.9,4);
+
+TestaMediana(amostra);
+
+void TestaMediana(Array array)
+{
+    if (array == null || array.Length == 0) 
+    {
+        Console.WriteLine("Array para cálculo da mediana está vazio ou nulo.");
+    }
+
+    double[] numerosOrdenados = (double[])array.Clone();
+    Array.Sort(numerosOrdenados);
+    //[1.4] [2] [5.1] 
+
+    int tamanho = numerosOrdenados.Length;
+    int meio = tamanho / 2;
+    double mediana = (tamanho % 2 != 0) ? numerosOrdenados[meio] :
+                                                    (numerosOrdenados[meio] + numerosOrdenados[meio - 1]) / 2;
+    Console.WriteLine($"Com base na amostra a mediana = {mediana}");
+}
+
+TestaMediana(amostra);
