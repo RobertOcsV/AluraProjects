317dfc1b6ad6a08f775bf6638a6281531993ad6c
Robert <61297366+RobertOcsV@users.noreply.github.com>
2022-10-17 15:57:11 -0300

    criando component de extrato

--- bytebank/src/app/app.module.ts
+++ bytebank/src/app/app.module.ts
@@ -5,11 +5,13 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { NovaTransferenciaComponents } from './nova-tranferencia/nova-transferencia.component';
 import { FormsModule } from '@angular/forms';
+import { ExtratoComponent } from './extrato/extrato.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NovaTransferenciaComponents
+    NovaTransferenciaComponents,
+    ExtratoComponent
   ],
   imports: [
     BrowserModule,
