96de52b439bb9a84069f12d4512131a56fe9df0e
Robert <61297366+RobertOcsV@users.noreply.github.com>
2022-10-18 15:38:47 -0300

    implementando os valores de extrato no component correto

--- bytebank/src/app/extrato/extrato.component.ts
+++ bytebank/src/app/extrato/extrato.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-extrato',
@@ -6,6 +6,10 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./extrato.component.css']
 })
 export class ExtratoComponent implements OnInit {
+
+
+
+  @Input() transferencia: any;
 
   constructor() { }
 
