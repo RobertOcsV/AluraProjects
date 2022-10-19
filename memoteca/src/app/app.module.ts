a48612267943d7cf944263292dda9e3a43f954ca
Robert <61297366+RobertOcsV@users.noreply.github.com>
2022-10-19 08:08:48 -0300

    começo do projeto memoteca angular + header

---
+++ memoteca/src/app/app.module.ts
@@ -0,0 +1,20 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { CabecalhoComponent } from './componentes/cabecalho/cabecalho.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    CabecalhoComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
