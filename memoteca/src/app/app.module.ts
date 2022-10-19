3ff7f6c27510d111cd5648bd9f0595ffdfe28fdd
Robert <61297366+RobertOcsV@users.noreply.github.com>
2022-10-19 08:17:03 -0300

    implementação do rodape no site

--- memoteca/src/app/app.module.ts
+++ memoteca/src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CabecalhoComponent } from './componentes/cabecalho/cabecalho.component';
+import { RodapeComponent } from './componentes/rodape/rodape.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    CabecalhoComponent
+    CabecalhoComponent,
+    RodapeComponent
   ],
   imports: [
     BrowserModule,
