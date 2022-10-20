63d452ad380c0a45e4109a7991a4d8e3be6052a6
Robert <61297366+RobertOcsV@users.noreply.github.com>
2022-10-20 15:31:33 -0300

    criado component de pensamento

---
+++ memoteca/src/app/componentes/pensamentos/pensamento/pensamento.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-pensamento',
+  templateUrl: './pensamento.component.html',
+  styleUrls: ['./pensamento.component.css']
+})
+export class PensamentoComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
