ad197a5eeb61e949e211b27f90fe55273f5edae8
Robert <61297366+RobertOcsV@users.noreply.github.com>
2023-05-01 17:31:37 -0300

    tratamento de erro em node

--- nodejs/index.js
+++ nodejs/index.js
@@ -1,12 +1,20 @@
 import fs from 'fs';
 import chalk from 'chalk';
 
+function trataErro(erro){
+    console.log(erro)
+    throw new Error(chalk.red(erro.code, 'irmao, deu pau aí, não tem arquivo no diretório'))
+}
+
 
 function pegaArquivo(caminhoDoArquivo){
     const encoding = 'utf-8';
-    fs.readFile(caminhoDoArquivo, encoding, (_, texto) =>{
+    fs.readFile(caminhoDoArquivo, encoding, (erro, texto) =>{
+        if(erro){
+            trataErro(erro);
+        }
         console.log(chalk.green(texto));
     } )
 }
 
-pegaArquivo('./arquivos/texto.md');
+pegaArquivo('./arquivos');
