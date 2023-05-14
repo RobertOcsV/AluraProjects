9af50d4419c457ce146f354cada3199684c8c3c8
Robert <61297366+RobertOcsV@users.noreply.github.com>
2023-05-14 17:24:16 -0300

    tratamento da busca de links

--- nodejs/src/cli.js
+++ nodejs/src/cli.js
@@ -1,5 +1,14 @@
+import chalk from 'chalk';
 import pegaArquivo from "./index.js";
+
 
 const caminho = process.argv;
 
-pegaArquivo(caminho[2]);
+
+async function processaTexto(caminho){
+    const resultado = await pegaArquivo(caminho[2]);
+    console.log(chalk.yellow('Lista de links'), resultado)
+
+}
+
+processaTexto(caminho);
