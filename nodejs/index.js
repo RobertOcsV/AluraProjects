ca85124ae262ef783f2e98fb2014311ed9c0beac
Robert <61297366+RobertOcsV@users.noreply.github.com>
2023-05-14 16:39:10 -0300

    regex

--- nodejs/index.js
+++ nodejs/index.js
@@ -1,22 +1,58 @@
 import fs from 'fs';
 import chalk from 'chalk';
 
+// const textoTeste = 'São geralmente recuperados a partir de um objeto [FileList](https://developer.mozilla.org/pt-BR/docs/Web/API/FileList) que é retornado como resultado da seleção, pelo usuário, de arquivos através do elemento [<input>](https://developer.mozilla.org/pt-BR/docs/Web/HTML/Element/Input), a partir do objeto [DataTransfer](https://developer.mozilla.org/pt-BR/docs/Web/API/DataTransfer) utilizado em operações de arrastar e soltar, ou a partir da API `mozGetAsFile()` em um [HTMLCanvasElement](https://developer.mozilla.org/pt-BR/docs/Web/API/HTMLCanvasElement). Em Gecko, códigos com privilégiios podem criar objetos File representando qualquer arquivo local sem a intereção do usuário (veja [Implementation notes](https://developer.mozilla.org/pt-BR/docs/Web/API/File#implementation_notes) para mais informações.).';
+
 function trataErro(erro){
     throw new Error(chalk.red(erro.code, 'irmao, deu pau aí, não tem arquivo no diretório'))
 }
+
+function extraiLinks(texto) {
+    const regex = /\[([^[\]]*?)\]\((https:\/\/[^\s?#.].[^\s]*)\)/gm;
+    const capturas = [...texto.matchAll(regex)];
+    const resultados = capturas.map(captura => ({[captura[1]]: captura[2]}))
+    return resultados;
+}
+
+
+
 
 async function pegaArquivo(caminhoDoArquivo){
 
     try{
     const encoding = 'utf-8';
     const texto = await fs.promises.readFile(caminhoDoArquivo, encoding)
-    console.log(chalk.green(texto));
+    console.log(extraiLinks(texto));
     } catch(erro) {
         trataErro(erro);
     } 
 }
 
-
 pegaArquivo('./arquivos/texto.md'); 
-setInterval(() => pegaArquivo('./arquivos/'), 5000);
+
+// function pegaArquivo(caminhoDoArquivo){
+//     const encoding = 'utf-8';
+//     fs.readFile(caminhoDoArquivo, encoding, (erro, texto) =>{
+//         if(erro){
+//             trataErro(erro);
+//         }
+//         console.log(chalk.green(texto));
+//     } )
+// }
+
+
+// function pegaArquivo(caminhoDoArquivo){
+//     const encoding = 'utf-8';
+//     fs.promises
+//         .readFile(caminhoDoArquivo, encoding)
+//         .then((texto)=>{console.log(chalk.green(texto))})
+//         .catch(trataErro)
+// }
+
+
+// setInterval(() => pegaArquivo('./arquivos/'), 5000);
+
+
+
+
 
