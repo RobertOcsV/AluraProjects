0024fc6d10287a66fc5611a545a1d188d28fb2bc
RobertOcsV <robertoliveiracor@hotmail.com>
2023-07-07 07:57:34 -0300

    component banner, className

--- reactfull/aluracurso1/organo/src/App.js
+++ reactfull/aluracurso1/organo/src/App.js
@@ -1,9 +1,11 @@
 import logo from './logo.svg';
 import './App.css';
+import Banner from './components/Banner/Banner';
 
 function App() {
   return (
     <div className="App">
+      <Banner></Banner>
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
---
+++ reactfull/aluracurso1/organo/src/components/Banner/Banner.js
@@ -0,0 +1,13 @@
+import './Banner.css'
+
+function Banner() {
+return(
+    //JSX
+    
+        <header className="banner">
+        <img src="\imagens\banner.png" alt="O banner principal da página do Organo"/>
+        </header>
+)
+}
+
+export default Banner
