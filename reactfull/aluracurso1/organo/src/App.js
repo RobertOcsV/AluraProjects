7e66bcca553ebba1c4e248010f815cda849e0e13
RobertOcsV <robertoliveiracor@hotmail.com>
2023-07-13 21:26:21 -0300

    Team component

--- reactfull/aluracurso1/organo/src/App.js
+++ reactfull/aluracurso1/organo/src/App.js
@@ -1,6 +1,7 @@
 import { useState } from 'react';
 import Banner from './components/Banner';
 import Formulario from './components/Formulario';
+import Time from './components/Time';
 
 function App() {
 
@@ -13,7 +14,9 @@ function App() {
     <div className="App">
       <Banner/>
       <Formulario aoColaboradorCadastrado={ colaborador => aoNovoColaboradorAdicionado(colaborador)}/>
-
+      <Time nome="Programação"></Time>
+      <Time nome="Frontend"></Time>
+      <Time nome="Data Science"></Time>
      </div> 
   );
 }
---
+++ reactfull/aluracurso1/organo/src/components/Time/Time.js
@@ -0,0 +1,11 @@
+import './Time.css'
+
+export const Time = (props) => {
+
+    return (
+        <section className='time'>
+            <h3>{props.nome}</h3>
+        </section>
+    )
+
+}
