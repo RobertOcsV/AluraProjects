350eae2f7b31cc2c073322e37329b1af6bc03415
RobertOcsV <robertoliveiracor@hotmail.com>
2023-08-14 10:29:29 -0300

    fest: test before aply jest in the node

--- unit_integrationJest/index.js
+++ unit_integrationJest/index.js
@@ -1,4 +1,32 @@
-const somaHorasExtras = (salario, valorHorasExtras) => salario
-+ valorHorasExtras;
+const somaHorasExtras = (salario, valorHorasExtras) => salario + valorHorasExtras;
 
-console.log(somaHorasExtras(2500, 500));
+const calculaDescontos = (salario, descontos) => salario - descontos;
+
+const verifiqueSe = (valor) => {
+  const assercoes = {
+    ehExatamenteIgualA(esperado) {
+      if (valor !== esperado) {
+        throw {};
+      }
+    },
+  };
+  return assercoes;
+};
+
+const teste = (titulo, funcaoDeTeste) => {
+  try {
+    funcaoDeTeste();
+    console.log(`${titulo} passsou!`);
+  } catch {
+    console.error(`${titulo} não passou!!!`);
+  }
+};
+
+teste('somaHorasExtras', () => {
+  const esperado = 2500;
+  const retornado = somaHorasExtras(2000, 500);
+
+  verifiqueSe(retornado).ehExatamenteIgualA(esperado);
+});
+
+teste('calculaDesconto', 2200, calculaDescontos(2500, 300));
