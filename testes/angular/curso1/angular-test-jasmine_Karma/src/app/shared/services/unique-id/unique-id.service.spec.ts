e3b9df56c5021688a830b337fbc5e8ade7bec765
RobertOcsV <robertoliveiracor@hotmail.com>
2023-08-23 13:40:33 -0300

    refactor: dynamic name test variable

--- testes/angular/curso1/angular-test-jasmine_Karma/src/app/shared/services/unique-id/unique-id.service.spec.ts
+++ testes/angular/curso1/angular-test-jasmine_Karma/src/app/shared/services/unique-id/unique-id.service.spec.ts
@@ -1,7 +1,7 @@
 import { UniqueIdService } from "./unidque-id.service";
 
-describe('UniqueService', () => {
-  it('#generateUniqueIdWithPrefix should generate id when called with prefix', () => {
+describe(UniqueIdService.name, () => {
+  it(`#${UniqueIdService.prototype.generateUniqueIdWithPrefix.name} should generate id when called with prefix`, () => {
       const service = new UniqueIdService();
       const id = service.generateUniqueIdWithPrefix('app');
       expect(id).toContain('app-')
