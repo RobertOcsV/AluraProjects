69bfb53f92b4a36b3086a63ad6347b16a74e8b73
RobertOcsV <robertoliveiracor@hotmail.com>
2023-08-23 13:45:30 -0300

    refactor: test UniqueIdService have most precision compare

--- testes/angular/curso1/angular-test-jasmine_Karma/src/app/shared/services/unique-id/unique-id.service.spec.ts
+++ testes/angular/curso1/angular-test-jasmine_Karma/src/app/shared/services/unique-id/unique-id.service.spec.ts
@@ -4,6 +4,6 @@ describe(UniqueIdService.name, () => {
   it(`#${UniqueIdService.prototype.generateUniqueIdWithPrefix.name} should generate id when called with prefix`, () => {
       const service = new UniqueIdService();
       const id = service.generateUniqueIdWithPrefix('app');
-      expect(id).toContain('app-')
+      expect(id.startsWith('app-')).toBeTrue();
   });
 });
