42580b7f03bcd2761aa887ce93a2a3337df80edd
RobertOcsV <robertoliveiracor@hotmail.com>
2023-08-23 10:10:46 -0300

    feat: make services for testing

---
+++ testes/angular/curso1/angular-test-jasmine_Karma/src/app/shared/services/unique-id/unidque-id.service.ts
@@ -0,0 +1,27 @@
+import { Injectable } from "@angular/core";
+import {  v4 as uuidv4 } from 'uuid'
+
+@Injectable()
+export class UniqueService {
+
+  private numberOfGeneratedIds = 0
+
+  public generateUniqueIdWithPrefix(prefix: string) : string {
+    if(!prefix){
+      throw Error('Prefix can not be empty')
+    }
+
+    const uniqueId = this.generateUniqueId();
+    this.numberOfGeneratedIds++;
+    return `${prefix}-${uniqueId}`
+  }
+
+  public getNumberOfGeneratedUniqueIds(): number {
+    return this.numberOfGeneratedIds;
+  }
+
+  private generateUniqueId(): string {
+    return uuidv4();
+  }
+
+}
