695eb6ab80e07ac059d8abf0c8ec5df4882c0647
RobertOcsV <robertoliveiracor@hotmail.com>
2023-08-26 17:38:58 -0300

    test: ensure id be unique with two keys

--- testes/angular/curso1/angular-test-jasmine_Karma/src/app/shared/services/unique-id/unique-id.service.spec.ts
+++ testes/angular/curso1/angular-test-jasmine_Karma/src/app/shared/services/unique-id/unique-id.service.spec.ts
@@ -6,4 +6,11 @@ describe(UniqueIdService.name, () => {
       const id = service.generateUniqueIdWithPrefix('app');
       expect(id.startsWith('app-')).toBeTrue();
   });
+
+  it(`#${UniqueIdService.prototype.generateUniqueIdWithPrefix.name} should generate id when called with prefix Should not generate duplicate IDs when called multiple times`, () => {
+      const service = new UniqueIdService();
+      const firstId = service.generateUniqueIdWithPrefix('app');
+      const secondId = service.generateUniqueIdWithPrefix('app');
+      expect(firstId).not.toBe(secondId)
+  });
 });
