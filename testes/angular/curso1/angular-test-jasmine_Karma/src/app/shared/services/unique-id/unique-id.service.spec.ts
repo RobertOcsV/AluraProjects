9ecaf04eaea27a6e16da787528ed024ce628ceeb
RobertOcsV <robertoliveiracor@hotmail.com>
2023-08-29 09:36:59 -0300

    feat: impelement test number os ids generateds called

--- testes/angular/curso1/angular-test-jasmine_Karma/src/app/shared/services/unique-id/unique-id.service.spec.ts
+++ testes/angular/curso1/angular-test-jasmine_Karma/src/app/shared/services/unique-id/unique-id.service.spec.ts
@@ -1,6 +1,14 @@
 import { UniqueIdService } from "./unidque-id.service";
 
+
+
 describe(UniqueIdService.name, () => {
+
+  let service: UniqueIdService = null;
+  beforeEach( () => {
+    service = new UniqueIdService();
+  })
+
   it(`#${UniqueIdService.prototype.generateUniqueIdWithPrefix.name} should generate id when called with prefix`, () => {
       const service = new UniqueIdService();
       const id = service.generateUniqueIdWithPrefix('app');
@@ -8,11 +16,17 @@ describe(UniqueIdService.name, () => {
   });
 
   it(`#${UniqueIdService.prototype.generateUniqueIdWithPrefix.name} should generate id when called with prefix Should not generate duplicate IDs when called multiple times`, () => {
-      const service = new UniqueIdService();
+
       const ids = new Set();
       for ( let i = 0; i < 50; i++){
         ids.add(service.generateUniqueIdWithPrefix('app'))
       }
       expect(ids.size).toBe(50);
+  });
+
+  it(`#${UniqueIdService.prototype.getNumberOfGeneratedUniqueIds.name} should return the number of generatedIds when called`, () => {
+    service.generateUniqueIdWithPrefix('app');
+    service.generateUniqueIdWithPrefix('app');
+    expect(service.getNumberOfGeneratedUniqueIds()).toBe(2);
   });
 });
