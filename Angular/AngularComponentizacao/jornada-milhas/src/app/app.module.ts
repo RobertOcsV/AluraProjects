479c5f3f369883f6db7989acfe2f03646ed49689
RobertOcsV <robertoliveiracor@hotmail.com>
2023-09-10 09:00:42 -0300

    feat: components commands

--- Angular/AngularComponentizacao/jornada-milhas/src/app/app.module.ts
+++ Angular/AngularComponentizacao/jornada-milhas/src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './shared/header/header.component';
+import { FooterComponent } from './shared/footer/footer.component';
+import { BannerComponent } from './shared/banner/banner.component';
+import { CardComponent } from './shared/card/card.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    BannerComponent,
+    CardComponent
   ],
   imports: [
     BrowserModule,
