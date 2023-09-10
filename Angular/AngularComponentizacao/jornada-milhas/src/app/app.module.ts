8e380e477adcfdbfa4312754f8dac655954ab151
RobertOcsV <robertoliveiracor@hotmail.com>
2023-09-10 09:11:42 -0300

    chore: install angular material

--- Angular/AngularComponentizacao/jornada-milhas/src/app/app.module.ts
+++ Angular/AngularComponentizacao/jornada-milhas/src/app/app.module.ts
@@ -7,6 +7,7 @@ import { HeaderComponent } from './shared/header/header.component';
 import { FooterComponent } from './shared/footer/footer.component';
 import { BannerComponent } from './shared/banner/banner.component';
 import { CardComponent } from './shared/card/card.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
@@ -18,7 +19,8 @@ import { CardComponent } from './shared/card/card.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
