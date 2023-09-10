5d9faaee7eed7f42f65866015ba9336a64c79967
RobertOcsV <robertoliveiracor@hotmail.com>
2023-09-10 13:23:26 -0300

    feat: banner component interpolation src/alt

--- Angular/AngularComponentizacao/jornada-milhas/src/app/shared/banner/banner.component.ts
+++ Angular/AngularComponentizacao/jornada-milhas/src/app/shared/banner/banner.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-banner',
@@ -6,5 +6,8 @@ import { Component } from '@angular/core';
   styleUrls: ['./banner.component.scss']
 })
 export class BannerComponent {
+
+@Input() src: string = ''
+@Input() alt: string = ''
 
 }
