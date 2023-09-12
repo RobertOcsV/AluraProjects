adeb87539218826440d59624734f7d56853225f2
RobertOcsV <robertoliveiracor@hotmail.com>
2023-09-12 11:26:04 -0300

    feat: input form group

--- Angular/AngularComponentizacao/jornada-milhas/src/app/app-material/app-material.module.ts
+++ Angular/AngularComponentizacao/jornada-milhas/src/app/app-material/app-material.module.ts
@@ -4,22 +4,26 @@ import { MatIconModule } from '@angular/material/icon';
 import { MatButtonModule } from '@angular/material/button';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatCardModule } from '@angular/material/card';
+import { MatButtonToggleModule } from '@angular/material/button-toggle';
+import { MatChipsModule } from '@angular/material/chips';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input'
 
 @NgModule({
   declarations: [],
   imports: [
     CommonModule,
-    MatToolbarModule,
-    MatButtonModule,
-    MatIconModule,
-    MatCardModule
   ],
   exports: [
     CommonModule,
     MatToolbarModule,
     MatButtonModule,
     MatIconModule,
-    MatCardModule
+    MatCardModule,
+    MatButtonToggleModule,
+    MatChipsModule,
+    MatFormFieldModule,
+    MatInputModule
   ]
 })
 export class AppMaterialModule { }
