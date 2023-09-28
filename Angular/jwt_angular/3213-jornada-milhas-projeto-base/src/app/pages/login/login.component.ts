72862d91cdc6029fa9b28d9d3a2e31196f106c51
RobertOcsV <robertoliveiracor@hotmail.com>
2023-09-28 16:36:29 -0300

    feat: formcontrol logincomponent

--- Angular/jwt_angular/3213-jornada-milhas-projeto-base/src/app/pages/login/login.component.ts
+++ Angular/jwt_angular/3213-jornada-milhas-projeto-base/src/app/pages/login/login.component.ts
@@ -1,10 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.scss']
 })
-export class LoginComponent {
+export class LoginComponent implements OnInit{
+  loginForm!: FormGroup;
+
+  constructor(private formBuilder: FormBuilder ){
+
+  }
+  ngOnInit(): void {
+    this.loginForm = this.formBuilder.group({
+      email: [null],
+      senha: [null ]
+    })
+  }
+
+  login(){
+    console.log('Login realizado com sucesso', this.loginForm.value)
+  }
+
 
 }
