54f599ebed4db40ed39e981ce5d38ed607a80536
RobertOcsV <robertoliveiracor@hotmail.com>
2023-10-01 20:41:58 -0300

    feat: validators conditions password

--- Angular/jwt_angular/3213-jornada-milhas-projeto-base/src/app/pages/login/login.component.ts
+++ Angular/jwt_angular/3213-jornada-milhas-projeto-base/src/app/pages/login/login.component.ts
@@ -3,39 +3,43 @@ import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
 import { AutentitacaoService } from 'src/app/core/services/autentitacao.service';
 
+
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.scss']
 })
-export class LoginComponent implements OnInit {
+export class LoginComponent implements OnInit{
+
   loginForm!: FormGroup;
 
-  constructor(private formBuilder: FormBuilder, private authService: AutentitacaoService, private router: Router) {
+  constructor(
+    private formBuilder: FormBuilder,
+    private authService: AutentitacaoService,
+    private router: Router
+  ) {}
 
-  }
   ngOnInit(): void {
     this.loginForm = this.formBuilder.group({
-      email: [null, Validators.required, Validators.email],
-      senha: [null, Validators.required]
+      email: [null, [Validators.required, Validators.email]],
+      senha: [null, [Validators.required, Validators.minLength(7)]]
     })
   }
 
   login() {
-    const email = this.loginForm.value.email;
-    const senha = this.loginForm.value.senha;
-
-    this.authService.autenticar(email, senha).subscribe({
-      next: (value) => {
-        console.log('Login realizado com sucesso', value)
-        this.router.navigateByUrl('/')
-      },
-      error: (err) => {
-        console.log('Erro no login', err)
-      }
-    })
-
+    if(this.loginForm.valid) {
+      const email = this.loginForm.value.email;
+      const senha = this.loginForm.value.senha;
+      this.authService.autenticar(email, senha).subscribe({
+        next: (value) => {
+          console.log('Autenticado com sucesso', value)
+          this.router.navigateByUrl('/')
+          this.loginForm.reset();
+        },
+        error: (err) => {
+          console.log('Problema na autenticação', err)
+        },
+      })
+    }
   }
-
-
 }
