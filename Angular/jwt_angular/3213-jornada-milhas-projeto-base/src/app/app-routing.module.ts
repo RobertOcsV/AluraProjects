81684f45083273e2f85de45c6f878a91aeea44e4
RobertOcsV <robertoliveiracor@hotmail.com>
2023-10-03 13:59:17 -0300

    feat: router cadastro page

--- Angular/jwt_angular/3213-jornada-milhas-projeto-base/src/app/app-routing.module.ts
+++ Angular/jwt_angular/3213-jornada-milhas-projeto-base/src/app/app-routing.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './pages/home/home.component';
 import { LoginComponent } from './pages/login/login.component';
+import { CadastroComponent } from './pages/cadastro/cadastro.component';
 
 const routes: Routes = [
   {
@@ -11,6 +12,10 @@ const routes: Routes = [
   {
     path: 'login',
     component: LoginComponent
+  },
+  {
+    path: 'cadastro',
+    component: CadastroComponent
   }
 ];
 
