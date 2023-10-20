2c382966823a82c9ea2b23d24822530357c3c497
RobertOcsV <robertoliveiracor@hotmail.com>
2023-10-20 15:32:10 -0300

    feat:methods cadastro patch and get

--- Angular/jwt_angular/3213-jornada-milhas-projeto-base/src/app/core/services/cadastro.service.ts
+++ Angular/jwt_angular/3213-jornada-milhas-projeto-base/src/app/core/services/cadastro.service.ts
@@ -1,5 +1,5 @@
 import { PessoaUsuaria } from './../types/type';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { environment } from 'src/environments/environment';
@@ -15,6 +15,19 @@ export class CadastroService {
   cadastrar(pessoaUsuaria: PessoaUsuaria): Observable<PessoaUsuaria>{
     return this.http.post<PessoaUsuaria>(`${this.apiUrl}/auth/cadastro`, pessoaUsuaria)
   }
+  buscarCadastro(token: string): Observable<PessoaUsuaria>{
+    const headers = new HttpHeaders({
+      'Authorization': `Bearer ${token}`
+    })
+    return this.http.get<PessoaUsuaria>(`${this.apiUrl}/auth/perfil`, { headers })
+  }
+  editarCadastro( pessoaUsuaria: PessoaUsuaria, token: string): Observable<PessoaUsuaria>{
+    const headers = new HttpHeaders({
+      'Authorization': `Bearer ${token}`
+    })
+    return this.http.patch<PessoaUsuaria>(`${this.apiUrl}/auth/perfil`, pessoaUsuaria,{ headers });
+  }
+
 }
 
 
