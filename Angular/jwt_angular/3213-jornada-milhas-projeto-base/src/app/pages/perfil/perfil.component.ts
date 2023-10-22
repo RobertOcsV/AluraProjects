114867138218adf8aaae6330fcd1ba3c318cfe37
RobertOcsV <robertoliveiracor@hotmail.com>
2023-10-22 10:32:44 -0300

    feat: login cadastro part1

--- Angular/jwt_angular/3213-jornada-milhas-projeto-base/src/app/pages/perfil/perfil.component.ts
+++ Angular/jwt_angular/3213-jornada-milhas-projeto-base/src/app/pages/perfil/perfil.component.ts
@@ -1,14 +1,53 @@
-import { Component } from '@angular/core';
+import { CadastroService } from './../../core/services/cadastro.service';
+import { Component, OnInit } from '@angular/core';
+import { FormGroup } from '@angular/forms';
+import { FormularioService } from 'src/app/core/services/formulario.service';
+import { TokenService } from 'src/app/core/services/token.service';
+import { PessoaUsuaria } from 'src/app/core/types/type';
 
 @Component({
   selector: 'app-perfil',
   templateUrl: './perfil.component.html',
   styleUrls: ['./perfil.component.scss']
 })
-export class PerfilComponent {
-  titulo = 'Olá pessoa';
+export class PerfilComponent implements OnInit {
+  titulo = 'Olá ';
   textoBotao = 'ATUALIZAR';
   perfilComponent = true;
+
+  nome = '';
+  token = '';
+  cadastro!: PessoaUsuaria;
+  form!: FormGroup<any> | null;
+
+  constructor(private tokenService: TokenService, private cadastroService: CadastroService, private formularioService: FormularioService){}
+
+  ngOnInit(): void {
+    this.token = this.tokenService.retornarToken();
+    this.cadastroService.buscarCadastro(this.token).subscribe(cadastro => {
+      this.cadastro = cadastro;
+      this.nome = this.cadastro.nome
+      this.carregarFormulario();
+    })
+  }
+
+  carregarFormulario() {
+    this.form = this.formularioService.getCadastro();
+    this.form?.patchValue({
+      id: this.cadastro.id,
+      nome: this.cadastro.nome ,
+      nascimento: this.cadastro.nascimento,
+      cpf: this.cadastro.cpf,
+      telefone: this.cadastro.telefone,
+      email: this.cadastro.email,
+      senha: this.cadastro.senha,
+      genero: this.cadastro.genero,
+      cidade: this.cadastro.cidade,
+      estado: this.cadastro.estado
+    })
+  }
+
+
 
   deslogar(){
     console.log('Logout realizado com sucesso')
