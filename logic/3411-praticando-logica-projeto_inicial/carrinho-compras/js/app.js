c9d106f0b4306ebad71458c9aee4dfc43a014f1f
RobertOcsV <robertoliveiracor@hotmail.com>
2023-11-12 19:58:37 -0300

    feat: new features llogic

--- logic/3411-praticando-logica-projeto_inicial/carrinho-compras/js/app.js
+++ logic/3411-praticando-logica-projeto_inicial/carrinho-compras/js/app.js
@@ -1,37 +1,37 @@
 const produtosCarrinho = [];
 
 function adicionar() {
-  let produtoSelecionado = document.getElementById('produto').value;
-  let nomeProduto = produtoSelecionado.split('-')[0];
-  let valorUnitario = parseFloat(produtoSelecionado.split('R$')[1]);
-  let quantidade = parseInt(document.getElementById('quantidade').value);
+  let produtoSelecionado = document.getElementById("produto").value;
+  let nomeProduto = produtoSelecionado.split("-")[0];
+  let valorUnitario = parseFloat(produtoSelecionado.split("R$")[1]);
+  let quantidade = parseInt(document.getElementById("quantidade").value);
 
+  if (quantidade > 0) {
+    const produtoExistente = produtosCarrinho.find(
+      (produto) => produto.nome === nomeProduto
+    );
+    if (produtoExistente) {
+      produtoExistente.quantidade += quantidade;
+    } else {
+      const novoProduto = {
+        nome: nomeProduto,
+        valorUnitario: valorUnitario,
+        quantidade: quantidade,
+      };
+      produtosCarrinho.push(novoProduto);
+    }
 
-  const produtoExistente = produtosCarrinho.find(produto => produto.nome === nomeProduto);
+    document.getElementById("quantidade").value = "";
 
-   if(produtoExistente.quantidade === ){
-      alert("insira a quantidade do produto")
-   }
-
-  if (produtoExistente) {
-    produtoExistente.quantidade += quantidade;
+    atualizarCarrinho();
   } else {
-  
-    const novoProduto = {
-      nome: nomeProduto,
-      valorUnitario: valorUnitario,
-      quantidade: quantidade
-    };
-    produtosCarrinho.push(novoProduto);
+    alert("Selecione a quantidade do produto");
   }
-
-
-  atualizarCarrinho();
 }
 
 function atualizarCarrinho() {
-  let carrinho = document.getElementById('lista-produtos');
-  carrinho.innerHTML = '';
+  let carrinho = document.getElementById("lista-produtos");
+  carrinho.innerHTML = "";
 
   for (const produto of produtosCarrinho) {
     let preco = produto.quantidade * produto.valorUnitario;
@@ -40,7 +40,6 @@ function atualizarCarrinho() {
         <span class="texto-azul">x${produto.quantidade}</span> ${produto.nome} <span class="texto-azul">R$${preco}</span>
       </section>`;
   }
-
 
   atualizarValorTotal();
 }
@@ -51,15 +50,12 @@ function atualizarValorTotal() {
     total += produto.quantidade * produto.valorUnitario;
   }
 
-
-  document.getElementById('valor-total').textContent = `R$${total.toFixed(2)}`;
+  document.getElementById("valor-total").textContent = `R$${total.toFixed(2)}`;
 }
 
 function limpar() {
-
   produtosCarrinho.length = 0;
 
- 
   atualizarCarrinho();
   atualizarValorTotal();
 }
