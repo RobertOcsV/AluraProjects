79802183d0a1a87947152fd0b17a0ae56bbe4317
RobertOcsV <robertoliveiracor@hotmail.com>
2023-11-19 12:15:37 -0300

    feat: error fixe

--- logic/3411-praticando-logica-projeto_inicial/ingresso/js/app.js
+++ logic/3411-praticando-logica-projeto_inicial/ingresso/js/app.js
@@ -1,19 +1,17 @@
-let qtdPistaElement = document.getElementById("qtd-pista").innerHTML = 100
-let qtdSuperiorElement = document.getElementById("qtd-superior").innerHTML = 200
-let qtdInferiorElement = document.getElementById("qtd-inferior").innerHTML = 400
+document.getElementById("qtd-pista").innerHTML = 100;
+document.getElementById("qtd-superior").innerHTML = 200;
+document.getElementById("qtd-inferior").innerHTML = 400;
 
 function comprar() {
   let tipoIngresso = document.getElementById("tipo-ingresso");
   let quantidadeIngressos = parseInt(document.getElementById("qtd").value);
-  let ingressoSelect =  document.getElementById(`qtd-${tipoIngresso.value}`)
+  let ingressoSelect = document.getElementById(`qtd-${tipoIngresso.value}`);
 
- if(ingressoSelect.textContent >= 0){
-  alert(`Quantidade de ingressos indisponível para ${tipoIngresso.value}`)
-  
- } else {
-  alert("Compra realizada")
-  ingressoSelect.innerHTML -= quantidadeIngressos
- }
-
+  if (ingressoSelect.textContent >= 0) {
+    alert("Compra realizada");
+    ingressoSelect.innerHTML -= quantidadeIngressos;
+    
+  } else {
+    alert(`Quantidade de ingressos indisponível para ${tipoIngresso.value}`);
+  }
 }
-
