909f117398ab2be4cdc35e4eaec223615b0927df
RobertOcsV <robertoliveiracor@hotmail.com>
2023-12-09 19:33:40 -0300

    feat: Custom error reactive form

--- Angular/ng14Evoluindo_Aplicacao/2438-angular-memoteca-aula-6/2438-angular-memoteca-aula-6/src/app/componentes/pensamentos/criar-pensamento/criar-pensamento.component.ts
+++ Angular/ng14Evoluindo_Aplicacao/2438-angular-memoteca-aula-6/2438-angular-memoteca-aula-6/src/app/componentes/pensamentos/criar-pensamento/criar-pensamento.component.ts
@@ -35,7 +35,7 @@ export class CriarPensamentoComponent implements OnInit {
   }
 
   criarPensamento() {
-    console.log(this.formulario.status)
+    console.log(this.formulario.get('autoria')?.errors)
     if(this.formulario.valid){
       this.service.criar(this.formulario.value).subscribe(() => {
         this.router.navigate(['/listarPensamento'])
