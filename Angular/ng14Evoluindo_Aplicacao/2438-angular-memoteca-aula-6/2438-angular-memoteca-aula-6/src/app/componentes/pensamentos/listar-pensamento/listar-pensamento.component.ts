324b4292d13cf08208f86c53cf735482d72e505f
RobertOcsV <robertoliveiracor@hotmail.com>
2023-12-13 12:57:08 -0300

    feat: class HttpParams

--- Angular/ng14Evoluindo_Aplicacao/2438-angular-memoteca-aula-6/2438-angular-memoteca-aula-6/src/app/componentes/pensamentos/listar-pensamento/listar-pensamento.component.ts
+++ Angular/ng14Evoluindo_Aplicacao/2438-angular-memoteca-aula-6/2438-angular-memoteca-aula-6/src/app/componentes/pensamentos/listar-pensamento/listar-pensamento.component.ts
@@ -10,11 +10,12 @@ import { Pensamento } from '../pensamento';
 export class ListarPensamentoComponent implements OnInit {
 
   listaPensamentos: Pensamento[] = [];
+  paginaAtual: number = 1
 
   constructor(private service: PensamentoService) { }
 
   ngOnInit(): void {
-    this.service.listar().subscribe((listaPensamentos) => {
+    this.service.listar(this.paginaAtual).subscribe((listaPensamentos) => {
       this.listaPensamentos = listaPensamentos
     })
   }
--- Angular/ng14Evoluindo_Aplicacao/2438-angular-memoteca-aula-6/2438-angular-memoteca-aula-6/src/app/componentes/pensamentos/pensamento.service.ts
+++ Angular/ng14Evoluindo_Aplicacao/2438-angular-memoteca-aula-6/2438-angular-memoteca-aula-6/src/app/componentes/pensamentos/pensamento.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpParams } from '@angular/common/http';
 import { Pensamento } from './pensamento';
 import { Observable } from 'rxjs';
 
@@ -16,8 +16,10 @@ export class PensamentoService {
   listar(pagina: number): Observable<Pensamento[]> {
     const itensPorPagina = 6;
 
-    // GET /posts?_page=7&_limit=20
-    return this.http.get<Pensamento[]>(`${this.API}?_page=${pagina}&_limit=${itensPorPagina}`)
+    let params = new HttpParams().set("_page", pagina).set("_limit", itensPorPagina)
+
+    // return this.http.get<Pensamento[]>(`${this.API}?_page=${pagina}&_limit=${itensPorPagina}`)
+    return this.http.get<Pensamento[]>(this.API, {params})
   }
 
   criar(pensamento: Pensamento): Observable<Pensamento> {
