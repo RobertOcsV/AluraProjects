419d5362c6391b1834c1b5c3c11589b60cc08a41
RobertOcsV <robertoliveiracor@hotmail.com>
2024-03-30 16:23:42 -0300

    feat: update database re-create database with migrations config

--- AluraCSharp/FormacaoCsharp/Curso1/ScreenSound/Banco/ScreenSoundContext.cs
+++ AluraCSharp/FormacaoCsharp/Curso1/ScreenSound/Banco/ScreenSoundContext.cs
@@ -10,7 +10,7 @@ namespace ScreenSound.Banco
            public DbSet<Artista> Artistas { get; set; }
            public DbSet<Musica> Musicas{ get; set; }
 
-        private string connectionString = "Data Source=(localdb)\\MSSQLLocalDB;Initial Catalog=ScreenSound;Integrated Security=True;Encrypt=False;Trust Server Certificate=False;Application Intent=ReadWrite;Multi Subnet Failover=False";
+        private string connectionString = "Data Source=(localdb)\\MSSQLLocalDB;Initial Catalog=ScreenSoundV1;Integrated Security=True;Encrypt=False;Trust Server Certificate=False;Application Intent=ReadWrite;Multi Subnet Failover=False";
 
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
