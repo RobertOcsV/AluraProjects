74dbc2512425250a8f37b54c06d671a7ce309c34
RobertOcsV <robertoliveiracor@hotmail.com>
2024-03-30 17:37:27 -0300

    feat: insert datas with migrations

--- AluraCSharp/FormacaoCsharp/Curso1/ScreenSound/Migrations/20240330201925_PopularTabela.Designer.cs
+++ AluraCSharp/FormacaoCsharp/Curso1/ScreenSound/Migrations/20240330201925_PopularTabela.Designer.cs
@@ -2,6 +2,7 @@
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using ScreenSound.Banco;
 
@@ -10,9 +11,11 @@ using ScreenSound.Banco;
 namespace ScreenSound.Migrations
 {
     [DbContext(typeof(ScreenSoundContext))]
-    partial class ScreenSoundContextModelSnapshot : ModelSnapshot
+    [Migration("20240330201925_PopularTabela")]
+    partial class PopularTabela
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
