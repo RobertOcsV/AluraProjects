e33e8e86abad1d768f5a682edb8224387871aa88
RobertOcsV <robertoliveiracor@hotmail.com>
2024-03-31 11:39:43 -0300

    feat: add datas table music

--- AluraCSharp/FormacaoCsharp/Curso1/ScreenSound/Modelos/Musica.cs
+++ AluraCSharp/FormacaoCsharp/Curso1/ScreenSound/Modelos/Musica.cs
@@ -9,6 +9,7 @@ internal class Musica
 
     public string Nome { get; set; }
     public int Id { get; set; }
+    public int? AnoLancamento { get; set; }
 
     public void ExibirFichaTecnica()
     {
