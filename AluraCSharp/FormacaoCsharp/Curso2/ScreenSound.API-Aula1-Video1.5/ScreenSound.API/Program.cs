280329770d1e36dd136f2c8f5a626d8cd8be23ca
RobertOcsV <robertoliveiracor@hotmail.com>
2024-04-03 13:44:05 -0300

    feat: error treatment not found 404

--- AluraCSharp/FormacaoCsharp/Curso2/ScreenSound.API-Aula1-Video1.5/ScreenSound.API/Program.cs
+++ AluraCSharp/FormacaoCsharp/Curso2/ScreenSound.API-Aula1-Video1.5/ScreenSound.API/Program.cs
@@ -10,13 +10,18 @@ var app = builder.Build();
 app.MapGet("/Artistas", () =>
 {
     var dal = new DAL<Artista>(new ScreenSoundContext());
-    return dal.Listar();
+    return Results.Ok(dal.Listar());
 });
 
 app.MapGet("/Artistas/{nome}", (string nome) =>
 {
     var dal = new DAL<Artista>(new ScreenSoundContext());
-    return dal.RecuperarPor(a => a.Nome.ToUpper().Equals(nome.ToUpper()));
+    var artista =  dal.RecuperarPor(a => a.Nome.ToUpper().Equals(nome.ToUpper()));
+    if (artista is null)
+    {
+        return Results.NotFound();
+    }
+    return Results.Ok(artista);
 });
 
 app.Run();
