38f59c427d51c667539b037bb0df1207e20b5d3a
RobertOcsV <robertoliveiracor@hotmail.com>
2024-04-03 12:31:16 -0300

    feat: conversor to json format search

--- AluraCSharp/FormacaoCsharp/Curso2/3506-csharpWeb-screensound-curso1-aula05-relacionamento/ScreenSound.API/Program.cs
+++ AluraCSharp/FormacaoCsharp/Curso2/3506-csharpWeb-screensound-curso1-aula05-relacionamento/ScreenSound.API/Program.cs
@@ -1,6 +1,17 @@
+using ScreenSound.Banco;
+using ScreenSound.Modelos;
+using System.Text.Json.Serialization;
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.Configure<Microsoft.AspNetCore.Http.Json.JsonOptions>(options => options.SerializerOptions.ReferenceHandler = ReferenceHandler.IgnoreCycles);
+
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+app.MapGet("/", () => {
+    var dal = new DAL<Artista>(new ScreenSoundContext());
+    return dal.Listar();
+
+});
 
 app.Run();
