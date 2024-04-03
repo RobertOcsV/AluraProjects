6b8c5cdf2e3075b4b34c06ecbe944e617af30e55
RobertOcsV <robertoliveiracor@hotmail.com>
2024-04-03 16:32:10 -0300

    feat: Delete method

--- AluraCSharp/FormacaoCsharp/Curso2/ScreenSound.API-Aula1-Video1.5/ScreenSound.API/Program.cs
+++ AluraCSharp/FormacaoCsharp/Curso2/ScreenSound.API-Aula1-Video1.5/ScreenSound.API/Program.cs
@@ -1,22 +1,25 @@
 using Microsoft.AspNetCore.Mvc;
 using ScreenSound.Banco;
 using ScreenSound.Modelos;
+using System.Data.SqlTypes;
 using System.Text.Json.Serialization;
 
 var builder = WebApplication.CreateBuilder(args);
 
+builder.Services.AddDbContext<ScreenSoundContext>();
+builder.Services.AddTransient<DAL<Artista>>();
+
 builder.Services.Configure<Microsoft.AspNetCore.Http.Json.JsonOptions>(options => options.SerializerOptions.ReferenceHandler = ReferenceHandler.IgnoreCycles);
 var app = builder.Build();
 
-app.MapGet("/Artistas", () =>
+app.MapGet("/Artistas", ([FromServices] DAL<Artista> dal) =>
 {
-    var dal = new DAL<Artista>(new ScreenSoundContext());
+
     return Results.Ok(dal.Listar());
 });
 
-app.MapGet("/Artistas/{nome}", (string nome) =>
+app.MapGet("/Artistas/{nome}", ([FromServices] DAL <Artista> dal, string nome) =>
 {
-    var dal = new DAL<Artista>(new ScreenSoundContext());
     var artista =  dal.RecuperarPor(a => a.Nome.ToUpper().Equals(nome.ToUpper()));
     if (artista is null)
     {
@@ -25,11 +28,20 @@ app.MapGet("/Artistas/{nome}", (string nome) =>
     return Results.Ok(artista);
 });
 
-app.MapPost("/Artistas", ([FromBody]Artista artista) =>
+app.MapPost("/Artistas", ([FromBody]Artista artista, [FromServices] DAL<Artista> dal) =>
 {
-    var dal = new DAL<Artista>(new ScreenSoundContext());
     dal.Adicionar(artista);
     return Results.Ok();
+});
+
+app.MapDelete("/Artistas/{id}", ([FromServices] DAL<Artista> dal, int id) =>
+{
+    var artista = dal.RecuperarPor(a => a.Id == id);
+    if (artista is null) {
+        return Results.NotFound();
+    }
+    dal.Deletar(artista);
+    return Results.NoContent();
 });
 
 app.Run();
