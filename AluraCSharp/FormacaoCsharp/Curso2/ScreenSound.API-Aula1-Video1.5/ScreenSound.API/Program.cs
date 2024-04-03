a74dcf305965906dbe5c8cc49123b31f3bd75584
RobertOcsV <robertoliveiracor@hotmail.com>
2024-04-03 15:12:28 -0300

    feat: post method test

--- AluraCSharp/FormacaoCsharp/Curso2/ScreenSound.API-Aula1-Video1.5/ScreenSound.API/Program.cs
+++ AluraCSharp/FormacaoCsharp/Curso2/ScreenSound.API-Aula1-Video1.5/ScreenSound.API/Program.cs
@@ -1,3 +1,4 @@
+using Microsoft.AspNetCore.Mvc;
 using ScreenSound.Banco;
 using ScreenSound.Modelos;
 using System.Text.Json.Serialization;
@@ -22,6 +23,13 @@ app.MapGet("/Artistas/{nome}", (string nome) =>
         return Results.NotFound();
     }
     return Results.Ok(artista);
+});
+
+app.MapPost("/Artistas", ([FromBody]Artista artista) =>
+{
+    var dal = new DAL<Artista>(new ScreenSoundContext());
+    dal.Adicionar(artista);
+    return Results.Ok();
 });
 
 app.Run();
