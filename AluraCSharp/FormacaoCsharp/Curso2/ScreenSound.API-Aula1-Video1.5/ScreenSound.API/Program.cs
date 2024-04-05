41d57920aaff58ee31a7a39273a2f4c368c1d688
RobertOcsV <robertoliveiracor@hotmail.com>
2024-04-04 22:42:50 -0300

    feat: crud musics

--- AluraCSharp/FormacaoCsharp/Curso2/ScreenSound.API-Aula1-Video1.5/ScreenSound.API/Program.cs
+++ AluraCSharp/FormacaoCsharp/Curso2/ScreenSound.API-Aula1-Video1.5/ScreenSound.API/Program.cs
@@ -1,11 +1,14 @@
 using Microsoft.AspNetCore.Mvc;
 using ScreenSound.Banco;
 using ScreenSound.Modelos;
-using System.Data.SqlTypes;
+
+
 using System.Text.Json.Serialization;
 
 var builder = WebApplication.CreateBuilder(args);
 
+
+builder.Services.AddTransient<DAL<Musica>>();
 builder.Services.AddDbContext<ScreenSoundContext>();
 builder.Services.AddTransient<DAL<Artista>>();
 
@@ -59,4 +62,53 @@ app.MapPut("/Artistas", ([FromServices] DAL<Artista> dal, [FromBody] Artista art
     return Results.Ok();
 
 });
+
+app.MapGet("/Musicas", ([FromServices] DAL<Musica> dal) =>
+{
+    return Results.Ok(dal.Listar());
+});
+
+app.MapGet("/Musicas/{nome}", ([FromServices] DAL<Musica> dal, string nome) =>
+{
+    var musica = dal.RecuperarPor(a => a.Nome.ToUpper().Equals(nome.ToUpper()));
+    if (musica is null)
+    {
+        return Results.NotFound();
+    }
+    return Results.Ok(musica);
+
+});
+
+app.MapPost("/Musicas", ([FromServices] DAL<Musica> dal, [FromBody] Musica musica) =>
+{
+    dal.Adicionar(musica);
+    return Results.Ok();
+});
+
+app.MapDelete("/Musicas/{id}", ([FromServices] DAL<Musica> dal, int id) => {
+    var musica = dal.RecuperarPor(a => a.Id == id);
+    if (musica is null)
+    {
+        return Results.NotFound();
+    }
+    dal.Deletar(musica);
+    return Results.NoContent();
+
+});
+
+app.MapPut("/Musicas", ([FromServices] DAL<Musica> dal, [FromBody] Musica musica) => {
+    var musicaAAtualizar = dal.RecuperarPor(a => a.Id == musica.Id);
+    if (musicaAAtualizar is null)
+    {
+        return Results.NotFound();
+    }
+    musicaAAtualizar.Nome = musica.Nome;
+    musicaAAtualizar.AnoLancamento = musica.AnoLancamento;
+
+    dal.Atualizar(musicaAAtualizar);
+    return Results.Ok();
+});
+
+
+
 app.Run();
