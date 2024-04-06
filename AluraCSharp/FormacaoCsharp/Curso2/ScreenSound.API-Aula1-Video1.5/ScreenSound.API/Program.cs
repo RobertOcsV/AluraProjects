37594c42b0d15bf6ba861fc084037f25918d1e8d
RobertOcsV <robertoliveiracor@hotmail.com>
2024-04-05 21:38:19 -0300

    feat: config swagger libs

--- AluraCSharp/FormacaoCsharp/Curso2/ScreenSound.API-Aula1-Video1.5/ScreenSound.API/Program.cs
+++ AluraCSharp/FormacaoCsharp/Curso2/ScreenSound.API-Aula1-Video1.5/ScreenSound.API/Program.cs
@@ -12,10 +12,16 @@ builder.Services.AddTransient<DAL<Musica>>();
 builder.Services.AddDbContext<ScreenSoundContext>();
 builder.Services.AddTransient<DAL<Artista>>();
 
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
 builder.Services.Configure<Microsoft.AspNetCore.Http.Json.JsonOptions>(options => options.SerializerOptions.ReferenceHandler = ReferenceHandler.IgnoreCycles);
 var app = builder.Build();
 
 app.AddEndPointsArtistas();
 app.AddEndPointsMusicas();
+
+app.UseSwagger();
+app.UseSwaggerUI();
 
 app.Run();
