43007e5a479bdd4d1dd03173da0590a4d85b2fa1
RobertOcsV <robertoliveiracor@hotmail.com>
2024-04-19 14:55:22 -0300

    feat: mublazor lib import and using

--- AluraCSharp/FormacaoCsharp/Curso3/ScreenSound-BlazorWeb-master/ScreenSound.Web/Program.cs
+++ AluraCSharp/FormacaoCsharp/Curso3/ScreenSound-BlazorWeb-master/ScreenSound.Web/Program.cs
@@ -1,11 +1,14 @@
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using MudBlazor.Services;
 using ScreenSound.Web;
 using ScreenSound.Web.Services;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
+
+builder.Services.AddMudServices();
 
 builder.Services.AddTransient<ArtistaAPI>();
 
