5cf0865280965717a4867487eae3179258aa4101
RobertOcsV <robertoliveiracor@hotmail.com>
2024-04-23 22:34:38 -0300

    feat: card musica cadastro com genero

--- AluraCSharp/FormacaoCsharp/Curso3/ScreenSound-BlazorWeb-master/ScreenSound.Web/Response/ArtistaResponse.cs
+++ AluraCSharp/FormacaoCsharp/Curso3/ScreenSound-BlazorWeb-master/ScreenSound.Web/Response/ArtistaResponse.cs
@@ -1,3 +1,9 @@
-﻿
-namespace ScreenSound.Web.Response;
-public record ArtistaResponse(int Id, string Nome, string Bio, string? FotoPerfil);
+﻿namespace ScreenSound.Web.Response;
+
+public record ArtistaResponse(int Id, string Nome, string Bio, string? FotoPerfil)
+{
+    public override string ToString()
+    {
+        return $"{Nome}";
+    }
+}
