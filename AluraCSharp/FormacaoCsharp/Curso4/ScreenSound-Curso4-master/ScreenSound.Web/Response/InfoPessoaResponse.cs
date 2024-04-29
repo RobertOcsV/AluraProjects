429599b1f06c8d34477452562182a575d5ef2c41
RobertOcsV <robertoliveiracor@hotmail.com>
2024-04-29 17:41:08 -0300

    feat: 3.1 State auth identity

---
+++ AluraCSharp/FormacaoCsharp/Curso4/ScreenSound-Curso4-master/ScreenSound.Web/Response/InfoPessoaResponse.cs
@@ -0,0 +1,9 @@
+﻿namespace ScreenSound.Web.Response
+{
+    public class InfoPessoaResponse
+    {
+        public string? Email { get; set; }
+        public bool isEmailConfirmed { get; set; }
+                
+    }
+}
--- AluraCSharp/FormacaoCsharp/Curso4/ScreenSound-Curso4-master/ScreenSound.Web/Services/AuthAPI.cs
+++ AluraCSharp/FormacaoCsharp/Curso4/ScreenSound-Curso4-master/ScreenSound.Web/Services/AuthAPI.cs
@@ -1,11 +1,35 @@
-﻿using ScreenSound.Web.Response;
+﻿using Microsoft.AspNetCore.Components.Authorization;
+using ScreenSound.Web.Response;
 using System.Net.Http.Json;
+using System.Security.Claims;
 
 namespace ScreenSound.Web.Services
 {
-    public class AuthAPI(IHttpClientFactory factory)
+    public class AuthAPI(IHttpClientFactory factory) : AuthenticationStateProvider
     {
         private readonly HttpClient _httpClient = factory.CreateClient("API");
+
+        public override async Task<AuthenticationState> GetAuthenticationStateAsync()
+        {
+            var pessoa = new ClaimsPrincipal();
+
+            var info = await _httpClient.GetFromJsonAsync<InfoPessoaResponse>("auth/manage/info");
+
+            if (info is not null)
+            {
+                Claim[] dados =
+                    [
+                        new Claim(ClaimTypes.Name, info.Email),
+                        new Claim(ClaimTypes.Email, info.Email)
+                    ];
+
+                var identity = new ClaimsIdentity(dados, "Cookies");
+                pessoa = new ClaimsPrincipal(identity);
+
+            }
+
+            return new AuthenticationState(pessoa);
+        }
 
         public async Task<AuthResponse> LoginAsync(string email, string senha)
         {
