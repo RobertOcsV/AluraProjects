25cafee3266aba9f1bc6e10056ae7d8558743c83
RobertOcsV <robertoliveiracor@hotmail.com>
2025-02-15 04:57:37 -0300

    feat: more test of tests

--- testes/formacaoTesteCsharp/3639-csharptestes-jornadamilhas-curso1/Jornada.Milhas.Test/OfertaViagemTest.cs
+++ testes/formacaoTesteCsharp/3639-csharptestes-jornadamilhas-curso1/Jornada.Milhas.Test/OfertaViagemTest.cs
@@ -1,3 +1,4 @@
+using JornadaMilhas.Modelos;
 using JornadaMilhasV1.Modelos;
 
 namespace Jornada.Milhas.Test
@@ -8,7 +9,7 @@ namespace Jornada.Milhas.Test
         public void TestandoOfertaValida()
         {
             //cenário
-            Rota rota = new Rota ("OrigemTeste", "DestinoTeste");
+            Rota rota = new Rota("OrigemTeste", "DestinoTeste");
             Periodo periodo = new Periodo(new DateTime(2024, 2, 1), new DateTime(2024, 2, 5));
             double preco = 100.0;
             var validacao = true;
@@ -34,5 +35,85 @@ namespace Jornada.Milhas.Test
             Assert.False(oferta.EhValido);
 
         }
+
+        [Fact]
+        public void TestandoOfertaComPeriodoInvalido()
+        {
+            //arrange
+            Rota rota = new Rota("OrigemTeste", "DestinoTeste");
+            Periodo periodo = new Periodo(new DateTime(2024, 2, 5), new DateTime(2024, 2, 1));
+            double preco = 100.0;
+
+            //act
+            OfertaViagem oferta = new OfertaViagem(rota, periodo, preco);
+
+            //assert
+            Assert.Contains("Erro: Data de ida não pode ser maior que a data de volta.", oferta.Erros.Sumario);
+            Assert.False(oferta.EhValido);
+        }
+
+        [Fact]
+        public void TesteNomeInicializadoCorretamente()
+        {
+            // Arrange
+            string nome = "Música Teste";
+
+            // Act
+            Musica musica = new Musica(nome);
+
+            // Assert
+            Assert.Equal(nome, musica.Nome);
+        }
+
+
+        [Fact]
+        public void TesteIdInicializadoCorretamente()
+        {
+            // Arrange
+            string nome = "Música Teste";
+            int id = 13;
+
+            // Act
+            Musica musica = new Musica(nome) { Id = id };
+
+            // Assert
+            Assert.Equal(id, musica.Id);
+        }
+
+        [Fact]
+        public void TesteToString()
+        {
+            // Arrange
+            int id = 1;
+            string nome = "Música Teste";
+            Musica musica = new Musica(nome);
+            musica.Id = id;
+            string toStringEsperado = @$"Id: {id} Nome: {nome}";
+
+            // Act
+            string resultado = musica.ToString();
+
+            // Assert
+            Assert.Equal(toStringEsperado, resultado);
+        }
+
+        [Fact]
+        public void TestantoPrecoNegativo()
+        {
+
+            //Arrange
+            Rota rota = new Rota("Origem1", "Destino1");
+            Periodo periodo = new Periodo(new DateTime(2024, 8, 20), new DateTime(2024, 8, 30));
+            double preco = -300.0;
+
+            //act
+            OfertaViagem oferta = new OfertaViagem(rota, periodo, preco);
+
+            //assert
+            Assert.Contains("O preço da oferta de viagem deve ser maior que zero.", oferta.Erros.Sumario);
+        
+        }
+
+
     }
 }
---
+++ testes/formacaoTesteCsharp/3639-csharptestes-jornadamilhas-curso1/src/JornadaMilhasV1/Modelos/Musica.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JornadaMilhas.Modelos
+{
+    public class Musica
+    {
+        public Musica(string nome)
+        {
+            Nome = nome;
+        }
+
+        public string Nome { get; set; }
+        public int Id { get; set; }
+        public string Artista { get; set; }
+
+        public void ExibirFichaTecnica()
+        {
+            Console.WriteLine($"Nome: {Nome}");
+
+        }
+
+        public override string ToString()
+        {
+            return @$"Id: {Id} Nome: {Nome}";
+        }
+    }
+}
